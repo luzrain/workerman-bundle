6b0616b251df537fc39194058582b41e02213f2c
Anton <anton.z@live.com>
2023-08-23 00:45:10 +0800

    nyholm/psr7 added as dev dependency

--- tests/App/Kernel.php
+++ tests/App/Kernel.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace Luzrain\WorkermanBundle\Test\App;
 
 use Luzrain\WorkermanBundle\WorkermanBundle;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -51,6 +56,12 @@ final class Kernel extends BaseKernel
             $container->autowire(Controller::class)->setAutoconfigured(true);
             $container->autowire(ScheduledJob::class)->setAutoconfigured(true);
             $container->autowire(Process::class)->setAutoconfigured(true);
+
+            $container->register('nyholm.psr7.psr17_factory', Psr17Factory::class);
+            $container->setAlias(ServerRequestFactoryInterface::class, 'nyholm.psr7.psr17_factory');
+            $container->setAlias(StreamFactoryInterface::class, 'nyholm.psr7.psr17_factory');
+            $container->setAlias(UploadedFileFactoryInterface::class, 'nyholm.psr7.psr17_factory');
+            $container->setAlias(ResponseFactoryInterface::class, 'nyholm.psr7.psr17_factory');
         });
     }
 
